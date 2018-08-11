e4423ebb49d5ac93378ce02d834eebf9141a58e8
Fred Duarte <fduartej@gmail.com>
2018-08-11 13:09:38 -0500

    lab02

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //for loop
+            Console.WriteLine("For Loop");
+            for(int y=0;y<10;y++){
+                Console.WriteLine("y {0}", y);
+            }
+
+            Console.WriteLine("While");
+            int i=0;
+            while(i<10){
+                Console.WriteLine("i {0}", i);
+                i++;
+            }
+            
+            Console.WriteLine("Do While");
+            int j=0;
+            do{
+                Console.WriteLine("j {0}", j);
+                j++;
+            }while (j<10);
+            
+            
+
+            Console.WriteLine("Increment");
+
+            int x=1;
+            
+            ++x;
+            Console.WriteLine("X {0}", x);
+            x++;    
+            Console.WriteLine("X {0}", x);
+
+
+            //variables
+            Console.WriteLine("Ingrese Cantidad:");
+            Int32 cantidad = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Precio:");
+            Int32 precio = Int32.Parse(Console.ReadLine());
+            Double total = cantidad * precio;
+            Console.WriteLine("Total {0}", total);
+            //if statement
+            if(total>50 && precio<7){
+                Console.WriteLine("Dar una promocion!");
+            }else{
+                Console.WriteLine("No tiene promocion!");
+            }
+        }
+    }
+}
